6377f1ccde6ffe26ef3881c23848c1afd505c91d
Tom Gibson <tgibson37@gmail.com>
2022-02-09 13:21:47 -0500

    parses alphas and numbers

--- src/tg37/tinyc/PT.java
+++ src/tg37/tinyc/PT.java
@@ -1,15 +1,15 @@
 /*	Parse tools: boolean symNameIs, int find, int mustFind, findEOS, rem.
- *	returned ints are indexes into pr. NEEDS TJ.pr and other TJ stuff.
+ *	returned ints are indexes into pr.
  */
 
 package tg37.tinyc;
 
-public class PT {
+public class PT extends TJ {
 
 /*	set error unless already set, capture cursor in errat */
 void eset( int err ){
-	if(TJ.error != 0){
-		TJ.error = err; TJ.errat = TJ.cursor;
+	if(error != 0){
+		error = err; errat = cursor;
 	}
 }
 //true if s1==s2 for length n
@@ -21,17 +21,18 @@ boolean match(String s1, String s2, int n) {
 }
 // true if cursor==token
 boolean matchcur(String token){
-	return matchN(token, TJ.cursor, token.length());
+	int len = tokenLength();
+	return token.equals(pr.subString(cursor,cursor+len-1));
 }
 // true if f/lname==token
 boolean matchnm(String token){
-	return matchN(token,TJ.fname,TJ.lname-TJ.fname+1);
+	return match(token,pr+fname,lname-fname+1);
 }
 
 /*	return true if symname matches arg, no state change 
  */
 	boolean symNameIs(String name){
-		int x = strncmp(TJ.pr[TJ.fname], name, TJ.lname-TJ.fname+1);
+		int x = strncmp(pr[fname], name, lname-fname+1);
 		return( x!=0 );
 	}
 
@@ -40,7 +41,7 @@ boolean matchnm(String token){
  */
 	int find( int from, int upto, char c) {
 		int x = from;
-		while( TJ.pr[x] != c && x<upto) {
+		while( pr[x] != c && x<upto) {
 			++x;
 		}
 		return x<upto ? x : 0;
@@ -49,7 +50,7 @@ boolean matchnm(String token){
 /*	same as find but sets err on no match 
  */
 	int mustFind( int from, int upto, char c, int err ) {
-		int x = find(TJ.pr[from], TJ.pr[upto], c);
+		int x = find(pr[from], pr[upto], c);
 		if( x ) return x;
 		else { eset(err); return 0; }
 	}
@@ -57,11 +58,11 @@ boolean matchnm(String token){
 /*	special find for end of string. Minds the old null ending.
  */
 	int findEOS( char x ) {
-		while( x<TJ.endapp) {
-			if( TJ.pr[x]==0 || TJ.pr[x]==0x22 ) return x;
+		while( x<endapp) {
+			if( pr[x]==0 || pr[x]==0x22 ) return x;
 			++x;
 		}
-		eset(TJ.CURSERR);
+		eset(CURSERR);
 		return 0;
 	}
 	
@@ -70,14 +71,14 @@ boolean matchnm(String token){
  */
 	void rem() {
 		for(;;) {
-			while(    TJ.pr[TJ.cursor]==0x0a
-					||TJ.pr[TJ.cursor]==0x0d
-					||TJ.pr[TJ.cursor]==' '
-					||TJ.pr[TJ.cursor]=='\t'
-				  )++TJ.cursor;
-			if( !(lit(TJ.xcmnt)||lit(TJ.xcmnt2)) ) return;
-			while( TJ.pr[TJ.cursor] != 0x0a && TJ.pr[TJ.cursor] != 0x0d && TJ.cursor<TJ.endapp )
-				++TJ.cursor;
+			while(    pr[cursor]==0x0a
+					||pr[cursor]==0x0d
+					||pr[cursor]==' '
+					||pr[cursor]=='\t'
+				  )++cursor;
+			if( !(lit(xcmnt)||lit(xcmnt2)) ) return;
+			while( pr[cursor] != 0x0a && pr[cursor] != 0x0d && cursor<endapp )
+				++cursor;
 		}
 	}
 }
