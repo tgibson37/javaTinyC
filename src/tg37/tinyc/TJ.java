ffe663710abbf30267958d0abd683e716b4e35e5
Tom Gibson <tgibson37@gmail.com>
2022-05-20 07:52:00 -0400

    composition instead of singletons, cleaner code, compiles.

--- src/tg37/tinyc/TJ.java
+++ src/tg37/tinyc/TJ.java
@@ -1,70 +1,78 @@
-/* Tiny-c, Java version: manifest constants and globals.
- */
 package tg37.tinyc;
 
-public class TJ {
-	public static void eset(int e){
-		error = e; errat = cursor;
-	}
-//sizes
-//static int STACKSIZE =    100;
-//static int PRSIZE    =  10000;
-
-/* Global data */
-static String pr;
-static int lpr, apr, endapp, prused, EPR;
-/* EPR is end of program SPACE. 
- *      pr starts with startSeed, then libs, then app, then values
- *      lpr is start of libraries
- *      apr is start of application program
- *      endapp is end of ALL program text, 
- *      endapp+10 start of value space
- *      prused includes values, moves up/down with fcn entry/leaving
- *      EPR is pointer to last byte of pr array
+/*	Tiny-c, Java version: manifest constants, globals, loadCode() and eset().
+	Base class for other components.
  */
-static int error;    // from list below. ZERO is good.
-static int errat;	 // where error occurred
-static boolean quiet;
-static int cursor;   // index into pr
-static int stcurs;	 // current statement
-static int fname,lname;  // most recently matched symbol
-static boolean leave;	// set true by return statement
-static boolean brake;	// set true by break statement
+public class TJ {
+// access to components...
+	public Expr exp;
+	public Stack stk;
+	public ST stmt;
+	public Vartab vt;
+	public Dialog dl;
+//	public TC tc;
+    public enum Type {CHAR, INT, FCN, STR };
+
+    public void eset(int e) {
+        error = e;
+        errat = cursor;
+    }
+
+    /* Global data */
+    String pr;
+    int lpr, apr, endapp, prused, EPR;
+    /* EPR is end of program SPACE.
+     *      pr starts with startSeed, then libs, then app, then values
+     *      lpr is start of libraries
+     *      apr is start of application program
+     *      endapp is end of ALL program text,
+     *      endapp+10 start of value space
+     *      prused includes values, moves up/down with fcn entry/leaving
+     *      EPR is pointer to last byte of pr array
+     */
+    int error;    // from list below. ZERO is good.
+    int errat;	 // where error occurred
+    boolean quiet;
+    int cursor;   // index into pr
+    int stcurs;	 // current statement
+    int fname,lname;  // most recently matched symbol
+    boolean leave;	// set true by return statement
+    boolean brake;	// set true by break statement
 // leave and brake are restored to false when their respective action is complete.
 
-/* type flags */
-static int CHAR = 0;
-static int INT  = 1;
-static int STRING = 2;
+    /* type flags */
+    final int CHAR = 0;
+    int INT  = 1;
+    int STRING = 2;
 
-/* error tags */
-static int STATERR =      1;
-static int CURSERR =      2;
-static int SYMERR  =      3;
-static int RPARERR =      5;
-static int RANGERR =      6;
-static int CLASERR =      7;
-static int TYPEERR =      8;
-static int SYNXERR =      9;
-static int LVALERR =      14;
-static int POPERR  =      15;
-static int PUSHERR =      16;
-static int TMFUERR =      17;
-static int TMVRERR =      18;
-static int TMVLERR =      19;
-static int LINKERR =      20;
-static int ARGSERR =      21;
-static int LBRCERR =      22;
-static int RBRCERR =      23;
-static int MCERR   =      24;
-static int EQERR   =      27;
-static int PTRERR  =      28;
-static int APPERR  =      29;
-static int DIVERR  =      30;
-static int VARERR  =      31;
+    /* error tags */
+    int STATERR =      1;
+    int CURSERR =      2;
+    int SYMERR  =      3;
+    int RPARERR =      5;
+    int RANGERR =      6;
+    int CLASERR =      7;
+    int TYPEERR =      8;
+    int SYNXERR =      9;
+    int LVALERR =      14;
+    int POPERR  =      15;
+    int PUSHERR =      16;
+    int TMFUERR =      17;
+    int TMVRERR =      18;
+    int TMVLERR =      19;
+    int LINKERR =      20;
+    int ARGSERR =      21;
+    int LBRCERR =      22;
+    int RBRCERR =      23;
+    int MCERR   =      24;
+    int EQERR   =      27;
+    int PTRERR  =      28;
+    int APPERR  =      29;
+    int DIVERR  =      30;
+    int VARERR  =      31;
 
-/* signals */
-static int EXIT =         98;
-static int KILL =         99;
+    /* signals */
+    int EXIT =         98;
+    int KILL =         99;
 
 }
--- src/tg37/tinyc/Var.java
+++ src/tg37/tinyc/Var.java
@@ -1,91 +1,108 @@
-/*	Variables and var tables: libs, globals, list of locals. 
- *	Search order is active local, globals, libs. 
+/*	Variables and var tables: libs, globals, list of locals.
+ *	Search order is active local, globals, libs.
  */
 package tg37.tinyc;
 import java.util.*;
 
 public class Var extends PT {
 //a variable
-	ST stmt = ST.getInstance();
-    String name; 
-    boolean isArray; 
-    Stuff.Type type; 
-    int len; 
+    String name;
+    boolean isArray;
+    Stuff.Type type;
+    int len;
     Stuff value;
-    Vartab vt = Vartab.getInstance();
-    
-	public String toString(){
-		return name+": "+value;
-	}
 
-/*********** var tools ****************/
-/* copy the argument value into the new local place */
-/*	int copyArgValue(struct var *v, int class, Type type, union stuff *passed ) {
-			if(passed && class){                                    /* passed pointer
-					(*v).value.up = (*passed).up;
-			} else if( passed && !class ) {                 /* passed datum
-					switch(type){
-					case Int:
-							put_int( (*v).value.up, (*passed).ui );
-							break;
-					case Char:
-							put_char( (*v).value.up, (*passed).uc );
-							break;
-					default:
-							eset(TYPEERR);
-							return TYPEERR;
-					}
-			}
-			return 0;
-	}
-*/
+    public String toString() {
+        return name+": "+value;
+    }
 
-/* SITUATION: Declaration is parsed, and its descriptive data known. Create
-	a Stuff and add it to curfun. Formerly newvar. */
-	public Var( boolean isArray, Stuff.Type type, int len, Stuff passed ) {
-		this.isArray = isArray;
-		this.type = type;
-		this.len  = len;
-		this.name = pr.substring(fname,lname);
-		if(passed!=null)this.value = passed.klone();   // function arg
-		else{     // declaration incl function parameter
-			if(type==Stuff.Type.INT){ this.value = Stuff.createIval(0); }
-			else if(type==Stuff.Type.CHAR){ this.value = Stuff.createCval((char)0); }
-			else if(type==Stuff.Type.FCN){ this.value = Stuff.createFvar(cursor); }
-		}
-		vt.curfun.put(name, this); 
-	}
+    /*********** var tools ****************/
+    /* copy the argument value into the new local place */
+    /*	int copyArgValue(struct var *v, int class, Type type, union stuff *passed ) {
+    			if(passed && class){                                    /* passed pointer
+    					(*v).value.up = (*passed).up;
+    			} else if( passed && !class ) {                 /* passed datum
+    					switch(type){
+    					case Int:
+    							put_int( (*v).value.up, (*passed).ui );
+    							break;
+    					case Char:
+    							put_char( (*v).value.up, (*passed).uc );
+    							break;
+    					default:
+    							eset(TYPEERR);
+    							return TYPEERR;
+    					}
+    			}
+    			return 0;
+    	}
+    */
+
+    /* SITUATION: Declaration is parsed, and its descriptive data known. Create
+    	a Stuff and add it to curfun. Formerly newvar. */
+    public Var( boolean isArray, Stuff.Type type, int len, Stuff passed ) {
+        this.isArray = isArray;
+        this.type = type;
+        this.len  = len;
+        this.name = pr.substring(fname,lname);
+        if(passed!=null)this.value = passed.klone();   // function arg
+        else {    // declaration incl function parameter
+            if(type==Type.INT) {
+                this.value = Stuff.createIval(0);
+            }
+            else if(type==Stuff.Type.CHAR) {
+                this.value = Stuff.createCval((char)0);
+            }
+            else if(type==Stuff.Type.FCN) {
+                this.value = Stuff.createFvar(cursor);
+            }
+        }
+        vt.curfun.put(name, this);
+    }
 
 
-// tests...
-	public static void main(String[] args){
-		System.out.println("running Var.main");
-		Vartab vt = Vartab.getInstance();
-		Stack stk = Stack.getInstance();
-pr="  L1   L2   G3   G4   a1   a2   b1   b2   ..   ..   ..   ..   ..   ..   ..   ";
+/* tests...
+    public static void main(String[] args) {
+        System.out.println("running Var.main");
+        Vartab vt = new Vartab();
+        Stack stk = new Stack();
+        pr="  L1   L2   G3   G4   a1   a2   b1   b2   ..   ..   ..   ..   ..   ..   ..   ";
 //  01234567890123456789012345678901234567890123456789012345678901234567890123456789
 //            1         2         3         4         5         6         7
-		vt.curfun = vt.libs;    // should get L1,L2
-		fname=2; lname=4;                        // L1
-		new Var(true, Stuff.Type.INT, 1, null);
-		for(int i=1; i<8; ++i ) {                // L2...b2
-			if(i==2) vt.curfun = vt.globals;			//  G3...G2
-			else if(i%2==0) {          // a1,a2 Then b1,b2 in second local frame
-				vt.newfun(); 
+        vt.curfun = vt.libs;    // should get L1,L2
+        fname=2;
+        lname=4;                        // L1
+        new Var(true, Stuff.Type.INT, 1, null);
+        for(int i=1; i<8; ++i ) {                // L2...b2
+            if(i==2) vt.curfun = vt.globals;			//  G3...G2
+            else if(i%2==0) {          // a1,a2 Then b1,b2 in second local frame
+                vt.newfun();
 //				vt.curfun=stk.peekTop();      // peekTop retrieves empty 2nd level
-			}
-			fname += 5; lname += 5;    // a1...b2
-			new Var(true, Stuff.Type.INT, 1, null);
-		}
-		vt.dumpVarTab();
-		Var v; Stuff s;
-		fname=2; lname=4; v=vt.addrval(); s= v.value;    // L1
-		System.out.println(s+" should be 0");
-		
-		fname=17; lname=19; v=vt.addrval(); s= v.value;   // G4
-		System.out.println(s+" should be 0");
-		
-		fname=32; lname=34;  v=vt.addrval(); s= v.value;   // b1
-		System.out.println(s+" should be 0");
-	}
+            }
+            fname += 5;
+            lname += 5;    // a1...b2
+            new Var(true, Stuff.Type.INT, 1, null);
+        }
+        vt.dumpVarTab();
+        Var v;
+        Stuff s;
+        fname=2;
+        lname=4;
+        v=vt.addrval();
+        s= v.value;    // L1
+        System.out.println(s+" should be 0");
+
+        fname=17;
+        lname=19;
+        v=vt.addrval();
+        s= v.value;   // G4
+        System.out.println(s+" should be 0");
+
+        fname=32;
+        lname=34;
+        v=vt.addrval();
+        s= v.value;   // b1
+        System.out.println(s+" should be 0");
+    }
+*/
 }
